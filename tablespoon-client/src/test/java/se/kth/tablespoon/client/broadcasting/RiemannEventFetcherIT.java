2fcb55b1dfbc9b43bcbb385b15fc09910f3681b9
hwesterb <hwesterb@kth.se>
2016-06-11 11:54:29 +0200

    Latency experiment on client-side.

--- tablespoon-client/src/test/java/se/kth/tablespoon/client/broadcasting/RiemannEventFetcherIT.java
+++ tablespoon-client/src/test/java/se/kth/tablespoon/client/broadcasting/RiemannEventFetcherIT.java
@@ -2,13 +2,13 @@ package se.kth.tablespoon.client.broadcasting;
 
 import io.riemann.riemann.client.RiemannClient;
 import java.io.IOException;
-import static org.junit.Assert.assertTrue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import se.kth.tablespoon.client.api.SubscriberTester;
-import se.kth.tablespoon.client.events.Comparator;
 import se.kth.tablespoon.client.events.EventType;
-import se.kth.tablespoon.client.events.Threshold;
 import se.kth.tablespoon.client.general.Group;
 import se.kth.tablespoon.client.topics.GroupTopic;
 import se.kth.tablespoon.client.topics.Topic;
@@ -18,45 +18,44 @@ import se.kth.tablespoon.client.util.Time;
 public class RiemannEventFetcherIT {
   
   static RiemannEventFetcher ref;
+  static Topic topic;
   static RiemannClient rClient;
   static SubscriberTester st;
   static double THRESHOLD = 10.3;
+  static final ThreadPoolExecutor tpe = new ThreadPoolExecutor(100, 100, 60, TimeUnit.SECONDS,
+      new LinkedBlockingQueue<Runnable>());
   
   @BeforeClass
   public static void setUpClass() throws IOException {
-    String uniqueId = "a0986132-a503-4cee-ae85-83ac17ddcec1";
+    String uniqueId = "a0986132-a503-4cee-ae85-83ac17ddcec";
     Group group = new Group("A");
     group.addMachine("1");
-    
-    Topic topic = new GroupTopic(
-        8,
+    topic = new GroupTopic(
+        0,
         Time.now(),
         uniqueId,
         EventType.REGULAR,
-        2,
+        1,
         group
     );
-    topic.setHigh(new Threshold(THRESHOLD, Comparator.LESS_THAN));
     System.out.println("Start this event on the agent:");
     topic.generateJson();
     System.out.println(topic.getJson());
     st = new SubscriberTester();
-    ref = new RiemannEventFetcher(st, topic, rClient);
     rClient = RiemannClient.tcp("localhost", 5555);
     rClient.connect();
     while(rClient.isConnected() == false) {
       System.out.println("Waiting for connection to be accepted.");
       Time.sleep(100);
     }
+    topic.createFetcher(st, rClient);
   }
   
-  
   @Test
-  public void test() throws IOException {
-    while(st.getCounter() < 2) {
-      if (ref.shouldQuery()) ref.query();
-      if (st.getEvent() != null) assertTrue(st.getEvent().getValue() < THRESHOLD);
-      Time.sleep(400);
+  public void testLatencyExperiment() throws IOException {
+    while(true) {
+      topic.fetch(tpe);
+      Time.sleep(1);
     }
   }
   
